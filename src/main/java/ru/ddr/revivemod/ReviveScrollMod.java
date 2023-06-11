b672b0b868d778e5a1f35536b0a06ddabf29a6eb
DDR <nikitatyravinin@gmail.com>
2023-06-11 13:18:40 +0700

    Add: add logger for debugging.

--- src/main/java/ru/ddr/revivemod/ReviveScrollMod.java
+++ src/main/java/ru/ddr/revivemod/ReviveScrollMod.java
@@ -10,12 +10,15 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.ddr.revivemod.Items.ReviveScrollItem;
 
 @Mod(ReviveScrollMod.MODID)
 public class ReviveScrollMod
 {
     public static final String MODID = "rm";
+    public static final Logger LOGGER = LogManager.getLogger(ReviveScrollMod.MODID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
     public static final RegistryObject<Item> REVIVESCROLL = ITEMS.register("revive_scroll", () -> new ReviveScrollItem(new Item.Properties().stacksTo(1))) ;
 
